0a38569052751c086e60bcce9fd7c0bb33263f19
will.doan <info.weTheBest@gmail.com>
2024-01-07 18:33:10 -0800

    add pagination component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { createClient } from "../prismicio";
 import { Layout } from "../components/Layout";
 import { Bounded } from "../components/Bounded";
 import { Article } from "../components/Article";
+import Pagination from "../components/Pagination";
 
 export async function generateMetadata() {
   const client = createClient();
@@ -14,15 +15,27 @@ export async function generateMetadata() {
   };
 }
 
-export default async function Index() {
-  const client = createClient();
+interface SearchParamsProps {
+  searchParams: { page: number, limit: number }
+}
 
-  const articles = await client.getAllByType("article", {
+export default async function Index({ searchParams }: SearchParamsProps) {
+  const client = createClient();
+  
+  // Pagination Params
+  let pageNum = searchParams.page ?? 1
+  let pageLimit = searchParams.limit ?? 2 // WIP
+
+  const result = await client.getByType("article", {
     orderings: [
       { field: "my.article.publishDate", direction: "desc" },
       { field: "document.first_publication_date", direction: "desc" },
-    ],
+    ],    
+    pageSize: pageLimit,
+    page: pageNum
   });
+  let articles = result.results
+  
   const navigation = await client.getSingle("navigation");
   const settings = await client.getSingle("settings");
 
@@ -38,6 +51,7 @@ export default async function Index() {
             <Article key={article.id} article={article} />
           ))}
         </ul>
+        <Pagination totalPage={result.total_pages}/>
       </Bounded>
     </Layout>
   );
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import clsx from 'clsx'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import React from 'react'
+
+interface PaginationProps {
+    totalPage: number
+}
+
+export default function Pagination({ totalPage } : PaginationProps) {
+    const router = useRouter()
+    const pathName = usePathname()
+    const searchParams = useSearchParams()
+    let pageNum: any = (searchParams.get("page")) ?? 1
+    
+    const handleNext = () => {
+        if (pageNum < totalPage) {
+            let nextPageNum = (typeof pageNum !== "number" ? parseInt(pageNum) : pageNum) + 1
+            const params = new URLSearchParams(searchParams)
+            params.set("page", nextPageNum.toString())
+
+            router.push(`${pathName}?${params}`, { scroll: false })
+        }
+    }
+
+    const handlePrev = () => {
+        if (pageNum> 1) {
+            let lastPageNum = (typeof pageNum !== "number" ? parseInt(pageNum) : pageNum) - 1
+            const params = new URLSearchParams(searchParams)
+            params.set("page", lastPageNum.toString())
+            router.push(`${pathName}?${params}`, { scroll: false })
+        }
+    }
+    
+    const handlePageClick = (index: number) => {
+        const params = new URLSearchParams(searchParams)
+        params.set('page', index.toString())
+        router.push(`${pathName}?${params}`, { scroll: false })
+    }
+
+    return (
+        <>
+            {
+                totalPage > 1 && <>                
+                    <div className='flex flex-row space-x-5 justify-center mt-10'>
+                        <button onClick={handlePrev} disabled={pageNum === 1} className={clsx((pageNum) == 1 && "text-gray-400 pointer-events-none")}>Prev</button>
+
+                        {
+                            Array.from({ length: totalPage }).map((_, index) => (
+                                <button 
+                                    key={`page-${index + 1}`}
+                                    className={clsx(pageNum == index + 1 && "text-gray-500 underline pointer-events-none")}
+                                    onClick={() => handlePageClick(index + 1)}
+                                >
+                                    {index + 1}
+                                </button>
+                            ))
+                        }
+
+                        <button onClick={handleNext} disabled={pageNum === totalPage} className={clsx((pageNum) == totalPage && "text-gray-400 pointer-events-none")}>Next</button>
+                    </div>
+                </>
+            }
+        </>
+    )
+}
