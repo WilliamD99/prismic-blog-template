bc876f41f0c6ec624d981e6d4acacff434b3b010
Will <dnam310199@gmail.com>
2024-08-04 15:37:07 -0700

    ignore ts error

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,15 +1,15 @@
+"use client";
 import { PrismicText } from "@prismicio/react";
-import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicNextLink } from "@prismicio/next";
 import * as prismic from "@prismicio/client";
 
 import { getExcerpt } from "../lib/getExcerpt";
-import { findFirstImage } from "../lib/findFirstImage";
 import { dateFormatter } from "../lib/dateFormatter";
 
 import { Heading } from "./Heading";
 
 // Types
-import { ArticleDocument } from "../../prismicio-types";
+import { ArticleDocument } from "../../types.generated";
 import Link from "next/link";
 
 const extractNameFromUID = (str: string) => {
@@ -19,19 +19,17 @@ const extractNameFromUID = (str: string) => {
 };
 
 export function Article({ article }: { article: ArticleDocument }) {
-  const featuredImage =
-    (prismic.isFilled.image(article.data.featuredImage) &&
-      article.data.featuredImage) ||
-    findFirstImage(article.data.slices);
   const date = prismic.asDate(
     article.data.publishDate || article.first_publication_date
   );
   const excerpt = getExcerpt(article.data.slices);
   let topics = article.data.topics;
   let short_desc = article.data.short_description;
-  console.log(topics);
   return (
-    <li className="test article w-4/5 rounded-lg py-5 px-8">
+    <li
+      className="test article w-4/5 rounded-lg py-5 px-8"
+      onClick={() => console.log(article)}
+    >
       <div className="grid grid-cols-1 gap-3 md:col-span-2 space-y-4">
         <Heading as="h4" size="3xl">
           <PrismicNextLink document={article}>
@@ -56,7 +54,7 @@ export function Article({ article }: { article: ArticleDocument }) {
             </div>
           )}
         </div>
-        {short_desc.length > 0 && <p>{short_desc[0].text}</p>}
+        {short_desc.length > 0 && <p>{short_desc[0]?.text}</p>}
       </div>
     </li>
   );
